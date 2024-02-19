dad57fd174a8398b360dc3beb0948c92b939f644
Jubair Ibn Khaled <160339037+Pro-jubair@users.noreply.github.com>
2024-02-19 15:15:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var num1 = prompt("Enter first number");
+var num2 = prompt("Enter second number");
+var num3 = prompt("Enter third number");
+
+if(num1 > num2 && num1 > num3)
+console.log("bigger number is = " + num1);
+
+else if(num2 > num1 && num2 > num3)
+console.log("bigger number is = " + num2);
+
+else 
+console.log("bigger number is = " + num3);
